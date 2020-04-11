4ce4624cb2c3d78892895d31430d9da7cae88ee5
rmaharashi <rmaharashi@gmail.com>
2020-04-11 23:42:43 +0530

    first update to git

---
+++ models/train_classifier.py
@@ -0,0 +1,101 @@
+import sys
+import re
+import nltk
+nltk.download('stopwords')
+import numpy as np
+import pandas as pd
+
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from nltk.corpus import stopwords
+from sqlalchemy import create_engine
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.metrics import classification_report
+from sklearn.externals import joblib
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+
+
+def load_data(database_filepath):
+    # load data from database
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql_table('DisasterData', engine)
+    X = df['message']
+    Y = df.iloc[:,4:]
+    category_names = df.columns[4:]
+    return X,Y,category_names
+
+def tokenize(text):
+    text = re.sub(r"[^a-zA-Z0-9]", ' ', text)
+    words = word_tokenize(text)
+    word_tokens = [w for w in words if w not in stopwords.words('english')]
+    clean_tokens = []
+    lemmatizer = WordNetLemmatizer()
+    for word in word_tokens:
+        clean_tok = lemmatizer.lemmatize(word.lower().strip())
+        clean_tokens.append(clean_tok)
+
+    return clean_tokens
+
+def build_model():
+    pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer=tokenize)),
+        ('tfidf', TfidfTransformer()),
+        ('clf', MultiOutputClassifier(RandomForestClassifier()))
+        ])
+    
+    parameters = {
+        'vect__max_df': (0.5, 0.75, 1.0),
+        'tfidf__use_idf': (True, False)
+    }
+    
+    cv = GridSearchCV(pipeline, parameters)
+    
+    return pipeline
+
+
+def evaluate_model(model, X_test, Y_test, category_names):
+    Y_pred = model.predict(X_test)
+    for i in range(Y_test.shape[1]):
+        print((Y_test[:i]==Y_pred[:i]).mean())
+          
+
+def save_model(model, model_filepath):
+    joblib.dump(model, model_filepath)
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, Y, category_names = load_data(database_filepath)
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, Y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, Y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
